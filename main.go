d9e13f1b72644b16cb36c46c2f2ea916e49cede1
Mike Sale <mike.sale@gmail.com>
2026-01-22 17:59:17 -0700

    refactor: move logger setup into logging.go

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"strings"
 	"syscall"
 	"time"
@@ -56,46 +55,6 @@ type LogEntry struct {
 	// Error Information
 	Error     string `json:"error,omitempty"`
 	ErrorType string `json:"error_type,omitempty"`
-}
-
-// setupLoggers initializes the loggers
-func setupLoggers(logDir string) (*log.Logger, *log.Logger, *log.Logger, error) {
-	if err := os.MkdirAll(logDir, 0755); err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to create log directory: %w", err)
-	}
-
-	successLogFile, err := os.OpenFile(
-		filepath.Join(logDir, "dnsres-success.log"),
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
-		0644,
-	)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to open success log file: %w", err)
-	}
-
-	errorLogFile, err := os.OpenFile(
-		filepath.Join(logDir, "dnsres-error.log"),
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
-		0644,
-	)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to open error log file: %w", err)
-	}
-
-	appLogFile, err := os.OpenFile(
-		filepath.Join(logDir, "dnsres-app.log"),
-		os.O_APPEND|os.O_CREATE|os.O_WRONLY,
-		0644,
-	)
-	if err != nil {
-		return nil, nil, nil, fmt.Errorf("failed to open app log file: %w", err)
-	}
-
-	successLog := log.New(successLogFile, "", log.LstdFlags)
-	errorLog := log.New(errorLogFile, "", log.LstdFlags)
-	appLog := log.New(appLogFile, "", log.LstdFlags)
-
-	return successLog, errorLog, appLog, nil
 }
 
 func main() {
